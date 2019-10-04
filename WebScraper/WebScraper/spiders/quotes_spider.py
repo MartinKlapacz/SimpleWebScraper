52f840840216455e90533557792d9033e7ef526f
Martin <klapacz.martin@gmail.com>
2019-10-04 21:08:10 +0200

    added text, author and tags extraction to quotes spider

--- WebScraper/WebScraper/spiders/quotes_spider.py
+++ WebScraper/WebScraper/spiders/quotes_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+import numpy
+import sklearn
+import pandas
 
 class Quotes_spider(scrapy.Spider):
     name = "quotes"
@@ -8,13 +11,21 @@ class Quotes_spider(scrapy.Spider):
             "http://quotes.toscrape.com/page/1/",
             "http://quotes.toscrape.com/page/2/",
         ]
-
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            text = quote.css('span.text::text').get()
+            author = quote.css('small.author::text').get()
+            tags = quote.css('div.tags a.tag::text').getall()
+            yield {
+                'text': text,
+                'author': author,
+                'tags': tags
+            }
+        
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
